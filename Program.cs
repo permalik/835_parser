f83bbe4fcd4447d918d18f0a9d20542e372ecd35
permalik <permalik@protonmail.com>
2025-02-24 14:49:04 -0600

    feat: impl parser and st/se validation

--- Program.cs
+++ Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main()
+    {
+        List<string[]> transactions = new();
+        List<string> currentTransaction = new();
+
+        using (FileStream fs = new FileStream("test.835", FileMode.Open, FileAccess.Read))
+        using (StreamReader reader = new StreamReader(fs))
+        {
+            string? line;
+            while ((line = await reader.ReadLineAsync()) != null)
+            {
+                if (line.StartsWith("ST*"))
+                {
+                    currentTransaction = new List<string>();
+                }
+
+                currentTransaction.Add(line);
+
+                if (line.StartsWith("SE*"))
+                {
+                    transactions.Add(currentTransaction.ToArray());
+                }
+            }
+        }
+
+        for (int i = 0; i < transactions.Count; i++)
+        {
+            bool hasN3 = false;
+            bool hasN4 = false;
+
+            foreach (var entry in transactions[i])
+            {
+                if (entry.StartsWith("N3*")) hasN3 = true;
+                if (entry.StartsWith("N4*")) hasN4 = true;
+            }
+
+            Console.WriteLine($"Transaction {i + 1}:");
+            foreach (var entry in transactions[i])
+            {
+                Console.WriteLine(entry);
+            }
+
+            Console.WriteLine($"Includes Address Information: {(hasN3 && hasN4 ? "Yes" : "No")}");
+            Console.WriteLine(new string('-', 40));
+        }
+    }
+}
+
--- obj/Debug/net9.0/835parser.AssemblyInfo.cs
+++ obj/Debug/net9.0/835parser.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("835parser")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8d6ccc963e20b8d6519f1ede41132d10637b7a8b")]
 [assembly: System.Reflection.AssemblyProductAttribute("835parser")]
 [assembly: System.Reflection.AssemblyTitleAttribute("835parser")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
